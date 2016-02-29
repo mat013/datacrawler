1a0610aeac5bacef5d485fb8c7e485bff8248275
Meang Akira Tanaka <meang.tnk@gmail.com>
2016-02-29 06:42:54 +0100

    test data

--- datadub/application/src/test/resources/source-create-data.sql
+++ datadub/application/src/test/resources/source-create-data.sql
@@ -2,10 +2,10 @@
 SET search_path TO src;
 
 insert into country(id, name)
-values(1, 'Denmark');
+values(1, United Kingdom');
 
 insert into zip(id, code, city, country_fk)
-values(1, '2000', 'Frederiksberg', 1);
+values(1, WC2N', 'London', 1);
 
 insert into address(id, street, houseidentifier, zip_fk)
-values(1, 'Helgesvej', '15', 1);
+values(1, 'Villiers Street', '51', 1);
