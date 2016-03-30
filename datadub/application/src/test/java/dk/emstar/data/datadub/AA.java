bb51614559e52288462f38ac25d062abd8dafc18
Meang Akira Tanaka <meang.tnk@gmail.com>
2016-03-30 19:57:09 +0200

    adding all

---
+++ datadub/application/src/test/java/dk/emstar/data/datadub/AA.java
@@ -0,0 +1,26 @@
+package dk.emstar.data.datadub;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+import static java.util.stream.Collectors.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.assertj.core.api.Fail.fail;
+import static org.mockito.Mockito.*;
+
+public class AA {
+
+	@Test
+	public void testName() throws Exception {
+		List<String> a;
+
+		
+		
+
+		
+	}
+	
+}
